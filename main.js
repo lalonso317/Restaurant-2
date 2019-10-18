832cbea7adb1595d3d3ea422699bc2f96ac57568
Luis Alonso <if.create702@gmail.com>
2019-10-18 16:51:47 -0700

    added body stuff

--- main.js
+++ main.js
@@ -1,27 +1,20 @@
 $(document).ready(function(){
-    var requeest = $.ajax({
+    var requesst = $.ajax({
         url: 'https://obscure-tundra-54269.herokuapp.com/bar-food',
         type: 'GET',
-        data:'json',
+        data:['appestizers','entrees','desserts']
     })   
         .done(function(requeest){
             $('.menu').html()
         })
     
     })
-    function initMap(){
-        var options = {
-          zoom: 15,
-          center:{lat:36.1699, lng: -115.1398}
-          }
-          var map = new google.maps.Map(document.getElementById("map"),options);
-
-          var marker = new google.maps.Marker({
-              postion: {lat:36.1699, lng: -115.1398},
-              map: map,
-              title: "NBW!",
-
-              
-      })
-            marker.setMap(map)
-      }
+    // function initMap(){
+    //     var options = {
+    //       zoom: 15,
+    //       center:{lat:36.1699, lng: -115.1398}
+    //       }
+    //       var map = new google.maps.Map(document.getElementById("map"),options);
+    
+    //         marker.setMap(map)
+    //   }
