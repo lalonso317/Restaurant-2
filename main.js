be3ab2d2042c0d4cd2464a6c6a36bf9cdb6257d5
Luis Alonso <if.create702@gmail.com>
2019-10-18 09:45:20 -0700

    map

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
 $(document).ready(function(){
-    $('#menu')('https://obscure-tundra-54269.herokuapp.com/bar-food').done(function(data){
-        console.log(data)
-    }) 
-})
+    var requeest = $.ajax({
+        url: 'https://obscure-tundra-54269.herokuapp.com/bar-food',
+        type: 'GET',
+        data:'json',
+    })   
+        .done(function(requeest){
+            $('.menu').html()
+        })
+    
+    })
+    function initMap(){
+        var options = {
+          zoom: 15,
+          center:{lat:36.1699, lng: -115.1398}
+          }
+          var map = new google.maps.Map(document.getElementById("map"),options);
+
+          var marker = new google.maps.Marker({
+              postion: {lat:36.1699, lng: -115.1398},
+              map: map,
+              title: "NBW!",
+
+              
+      })
+            marker.setMap(map)
+      }
