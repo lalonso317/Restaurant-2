e3286456751ca0d23deddf9d4ddb28ddeb8cb6df
Luis Alonso <if.create702@gmail.com>
2019-10-20 15:57:36 -0700

    adding the test branch

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ $(document).ready(function(){
     var app = data.appetizers
     var entre = data.entre 
     var des = data.desserts
-
+ 
 var html = `<div class="appetizers"> </div>`
-
+ 
       html += app.map(ap => {
       return `
       <p class="appname">name: ${ap.name}</p>
@@ -13,25 +13,48 @@ var html = `<div class="appetizers"> </div>`
       <p class="appdescr">description: ${ap.description}</p>
       <p class="appext">extra: ${ap.extra}<p>
   `
-  console.log(html)
     }).join('')
     $('.appetizers').html(html)
+  
+   var html2 = `<div class="entrees"> </div>`
+ 
+   html2 += entre.map(ap => {
+    return `
+    <p class="entrname">${ap.name}</p>
+    <p class="entpri">${ap.price}</p>
+    <p class="entdescr">${ap.description}</p>
+    <p class="entext">${ap.extra}</p>
+    `
+   }).join('')
+   $('.entrees').html(html)
+ 
+   var html3 = `<div class="desserts"></div>`
+ 
+   html3 += des.map(ap => {
+    return `
+    <p class="desname">${ap.name}</p>
+    <p class="despri">${ap.price}</p>
+    <p class="desdescr">${ap.description}</p>
+    <p class="desext">${ap.extra}</p>
+    `
+   }).join('')
+    $('.desserts').html(html)
   })
 }) 
  
-
-
+ 
+ 
 var slideIndex = 1
 showDivs(slideIndex)
-
+ 
 function plusDivs(n) {
   showDivs((slideIndex += n))
 }
-
+ 
 function currentDiv(n) {
   showDivs((slideIndex = n))
 }
-
+ 
 function showDivs(n) {
   var i
   var x = document.getElementsByClassName("mySlides")
@@ -51,4 +74,3 @@ function showDivs(n) {
   x[slideIndex - 1].style.display = "block"
   dots[slideIndex - 1].className += " w3-white"
 }
-
